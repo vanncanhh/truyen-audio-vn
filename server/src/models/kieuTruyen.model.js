d7e28c1942ee531f36886c8b12bde89c1efbd39d
VanCanh1102 <nvc11022003@gmail.com>
2025-07-17 23:34:41 +0700

    init react

--- server/src/models/kieuTruyen.model.js
+++ server/src/models/kieuTruyen.model.js
@@ -5,6 +5,40 @@ const KieuTruyen = {
     const [rows] = await db.query("select * from KieuTruyen");
     return rows;
   },
+  getById: async (id) => {
+    const [rows] = await db.query("SELECT * FROM KieuTruyen WHERE id = ?", [
+      id,
+    ]);
+    return rows[0];
+  },
+
+  create: async (data) => {
+    const { TenKieuTruyen } = data;
+
+    const [result] = await db.query(
+      `INSERT INTO KieuTruyen 
+          (TenKieuTruyen)
+          VALUES (?)`,
+      [TenKieuTruyen]
+    );
+
+    return result.insertId;
+  },
+
+  update: async (id, data) => {
+    const [result] = await db.query("UPDATE KieuTruyen SET ? WHERE id = ?", [
+      data,
+      id,
+    ]);
+    return result.affectedRows;
+  },
+
+  delete: async (id) => {
+    const [result] = await db.query("DELETE FROM KieuTruyen WHERE id = ?", [
+      id,
+    ]);
+    return result.affectedRows;
+  },
 };
 
 module.exports = KieuTruyen;
--- server/src/services/kieuTruyen.service.js
+++ server/src/services/kieuTruyen.service.js
@@ -4,6 +4,17 @@ const KieuTruyenService = {
   getAllKieuTruyen: async () => {
     return await KieuTruyen.getAll();
   },
+
+  getByIdKieuTruyen: async (id) => {
+    const kieuTruyen = await KieuTruyen.getById(id);
+    if (!kieuTruyen) throw new Error("Kieu truyen not found");
+    return kieuTruyen;
+  },
+
+  createKieuTruyen: async (data) => {
+    if (!data.TenKieuTruyen) throw new Error("Ten Kieu Truyen is required");
+    return await KieuTruyen.create(data);
+  },
 };
 
 module.exports = KieuTruyenService;
